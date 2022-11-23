4615e7fc0db72fceccedf639b72e91ed65280c69
pixel-Teee <1173769234@qq.com>
2022-11-23 23:45:52 +0800

    add glfw and vulkan

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,34 @@
-#include <iostream>
+#include <PreCompileHeader.h> //pch
+
+//------vulkan for glfw------
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+//------vulkan for glfw------
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    //std::cout << "Hello World" << std::endl;
+
+    //------init glfw------
+    GLFWwindow* window;
+
+    if (!glfwInit())
+    {
+        std::cout << "create glfw window error!" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);//don't create opengl context
+
+    window = glfwCreateWindow(800, 600, "window", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        //draw frame
+    }
+
+    //------init glfw------
     return 0;
 }
